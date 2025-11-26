d830658e3814f86b5b9e1101632e9195bdb2a57a
Dmytro Prokopenko <portal200385@gmail.com>
2025-11-26 21:57:55 +0100

    feat(app): implement console entry point and DI container

--- src/ServerSimulator.Application/Program.cs
+++ src/ServerSimulator.Application/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ServerSimulator.Library.Configurations;
+using ServerSimulator.Library.Factories;
+using ServerSimulator.Library.Logging;
+using ServerSimulator.Library.Services;
+using ServerSimulator.Library.Services.ServerWorkloadManager;
+using ServerSimulator.Library.Services.SnapshotService;
+using ServerSimulator.Library.Validators;
+
+var services = new ServiceCollection();
+
+services.AddLogging(c => c.AddConsole());
+services.AddTransient<IServerConfigurationProvider, ServerConfigurationProvider>();
+services.AddTransient<IServerFactory, ServerFactory>();
+services.AddTransient<IServerWorkloadManager, ServerWorkloadManager>();
+services.AddTransient<ServerInitializer>();
+services.AddTransient<ServerManager>();
+services.AddTransient<IValidator<ServerConfiguration>, ServerConfigurationValidator>();
+services.AddSingleton<SnapshotLogger>();
+services.AddSingleton<ISnapshotService>(sp => 
+    new SnapshotService(sp.GetRequiredService<SnapshotLogger>(), TimeSpan.FromSeconds(3)));
+services.AddTransient<ServerSimulator.Application.ServerSimulator>();
+
+var provider = services.BuildServiceProvider();
+var app = provider.GetRequiredService<ServerSimulator.Application.ServerSimulator>();
+
+Console.WriteLine("Starting simulation...");
+
+string configPath = "servers_config.json"; 
+if(File.Exists(configPath))
+{
+    app.Initialize(configPath);
+    await app.RunSimulation(50, 3000);
+}
+else
+{
+    Console.WriteLine("Config file not found. Please create servers_config.json");
+}
---
+++ src/ServerSimulator.Application/ServerSimulator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using ServerSimulator.Library.Entities;
+using ServerSimulator.Library.Services;
+using ServerSimulator.Library.Services.SnapshotService;
+
+namespace ServerSimulator.Application;
+
+public class ServerSimulator(
+    ILogger<Server> serverLogger,
+    ServerInitializer initializer,
+    ISnapshotService snapshotService,
+    ServerManager serverManager)
+{
+    private List<Server> _servers = [];
+
+    public void Initialize(string path)
+    {
+        _servers = initializer.InitializeServers(path, serverLogger);
+        snapshotService.Start(_servers);
+    }
+
+    public async Task RunSimulation(int players, int duration)
+    {
+        if (_servers.Count == 0) return;
+        await serverManager.SimulateConnectionsAsync(_servers, players, duration);
+        await Task.Delay(2000); // Wait for logs
+    }
+}
